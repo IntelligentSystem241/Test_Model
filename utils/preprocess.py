e7b1f1377a678f8f877994eb45800b4f340e3da6
IntelligentSystem241 <intelligentsystem.sdh241@gmail.com>
2025-05-04 23:03:46 +0700

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -3,6 +3,6 @@ import pandas as pd
 def clean_data(df):
     df = df[['SID', 'LAT', 'LON', 'ISO_TIME', 'STORM_SPEED', 'STORM_DIR', 'DIST2LAND']]
     df = df.dropna()
-    df['ISO_TIME'] = pd.to_datetime(df['ISO_TIME'])
+    df['ISO_TIME'] = pd.to_datetime(df['ISO_TIME'], errors='coerce')
     df = df.sort_values(by=['SID', 'ISO_TIME'])
     return df
