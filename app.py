f56e37368ffc15dec7c43b496b85c7d9bd77702c
IntelligentSystem241 <intelligentsystem.sdh241@gmail.com>
2025-05-08 07:28:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def predict():
 
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render sẽ đặt biến PORT
+    port = int(os.environ.get("PORT", 10000))  # Render sẽ đặt biến PORT
     app.run(host="0.0.0.0", port=port)
 
