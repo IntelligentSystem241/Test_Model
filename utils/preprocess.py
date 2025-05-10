160bb071da8f949db2419d85b8581309af588a13
IntelligentSystem241 <intelligentsystem.sdh241@gmail.com>
2025-05-10 14:44:37 +0700

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -3,7 +3,7 @@ import pandas as pd
 def clean_data(df):
     df = df[['SID', 'LAT', 'LON', 'ISO_TIME', 'STORM_SPEED', 'STORM_DIR', 'DIST2LAND']]
     df = df.dropna()
-    df['ISO_TIME'] = pd.to_datetime(df['ISO_TIME'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+    df['ISO_TIME'] = pd.to_datetime(df['ISO_TIME'], format='%m/%d/%Y %H:%M', errors='coerce')
     df['LAT'] = pd.to_numeric(df['LAT'], errors='coerce')
     df['LON'] = pd.to_numeric(df['LON'], errors='coerce')
 
