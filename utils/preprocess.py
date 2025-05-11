bf6bc5ffef0a7cdb73fbaa7b61b9ff0f54e42101
IntelligentSystem241 <intelligentsystem.sdh241@gmail.com>
2025-05-11 10:10:15 +0700

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,16 +1,12 @@
-import pandas as pd
-
 def clean_data(df):
     df = df[['SID', 'LAT', 'LON', 'ISO_TIME', 'STORM_SPEED', 'STORM_DIR', 'DIST2LAND']]
     df = df.dropna()
     df['ISO_TIME'] = pd.to_datetime(df['ISO_TIME'], format='%m/%d/%Y %H:%M', errors='coerce')
     df['LAT'] = pd.to_numeric(df['LAT'], errors='coerce')
     df['LON'] = pd.to_numeric(df['LON'], errors='coerce')
-
-    df = df.dropna(subset=['LAT', 'LON'])
-
+    df['STORM_SPEED'] = pd.to_numeric(df['STORM_SPEED'], errors='coerce')
+    df['STORM_DIR'] = pd.to_numeric(df['STORM_DIR'], errors='coerce')
+    df['DIST2LAND'] = pd.to_numeric(df['DIST2LAND'], errors='coerce')
+    df = df.dropna()
     df = df.sort_values(by=['SID', 'ISO_TIME'])
-        # In 10 dòng đầu tiên để kiểm tra ISO_TIME
-    print("\n[DEBUG] ISO_TIME sau khi xử lý:")
-    print(df[['SID', 'ISO_TIME']].head(10))
     return df
